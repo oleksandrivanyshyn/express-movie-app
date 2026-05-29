f5f423c4a77200cc74cb44b9a18687a51f1bc231
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-29 15:29:22 +0300

    feat: add movie and search routes, update API key, and enhance server configuration

--- movieApi/app.js
+++ movieApi/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const movieRouter = require('./routes/movie');
+const searchRouter = require('./routes/search');
 
 var app = express();
 app.use(helmet());
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movies', movieRouter);
+app.use('/search', searchRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- movieApi/routes/index.js
+++ movieApi/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/most_popular', (req, res, next) => {
+  let page = req.query.page || 1;
+  if(req.query.api_key !== '123456789'){
+    res.json('invalid api key');
+  }
+  let results = movies.filter(movie => movie.most_popular);
+  results = results.slice((page - 1) * 20, page * 20);
+  res.json({results, page})
+})
 module.exports = router;
