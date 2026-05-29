9e3c1eea34a8f512a2f2bff3ba2ae90c5e2dd777
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-29 16:27:18 +0300

    feat: add query validation and search endpoints for movies and people

--- movieApi/routes/search.js
+++ movieApi/routes/search.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+const movies = require('../data/movies');
+const people = require('../data/people');
+
+function queryRequired(req, res, next){
+    const searchTerm = req.query.query;
+    if(!searchTerm){
+        return res.json({msg: "Query is required."});
+    }
+
+    next();
+}
+
+router.use(queryRequired)
+
+router.get('/movie',(req, res, next)=>{
+    const searchTerm = req.query.query;
+    const results = movies.filter((movie)=>{
+        let found = movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+        return found;
+    })
+    res.json({results})
+})
+
+router.get('/person',(req, res, next)=>{
+    const searchTerm = req.query.query;
+    const results = people.filter((person)=>{
+        let found = person.name.includes(searchTerm);
+        return found;
+    })
+    res.json({results})
+})
+
 
 module.exports = router;
